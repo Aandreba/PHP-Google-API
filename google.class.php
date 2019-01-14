a67192299069e6abb222bba5031bf40348b07ffe
Alex <aandreba@gmail.com>
2019-01-14 16:29:42 +0100

    Update google.class.php

--- google.class.php
+++ google.class.php
@@ -1,9 +1,33 @@
 <?php
 class GoogleSearch {
+  
+  private function getElementByAttribute(&$parentNode, $attr, $tagName, $attrVal) {
+    $response = false;
+
+    $childNodeList = $parentNode->getElementsByTagName($tagName);
+    $tagCount = 0;
+    for ($i = 0; $i < $childNodeList->length; $i++) {
+        $temp = $childNodeList->item($i);
+        if (stripos($temp->getAttribute($attr), $attrVal) !== false) {
+             $response[] = $temp;
+
+            $tagCount++;
+        }
+
+    }
+
+    return $response;
+}
+  
   function __construct(string $q){
     $this->q = $q;
     $this->url = "https://www.google.com/search?q=".$this->q;
-    
+    $this->DOM = new DOMDocument();
+    $this->DOM->loadHTML(file_get_contents($this->url));
+  }
+  
+  function iWillBeLucky(){
+    return $this->getElementByAttribute($DOM, "class", "div", "g")[0];
   }
   
 ?>
